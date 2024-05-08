9ed1da899fcc6d2e375936ef1d9f3832550326fb
Alex SHD <alex@shdlabs.com>
2024-05-08 14:22:41 +0300

    BidService handler, json return object => passing tests

--- services/bidding/api.go
+++ services/bidding/api.go
@@ -1,7 +1,25 @@
 package bidding
 
-import "log/slog"
+import (
+	"encoding/json"
+	"math/rand/v2"
+	"net/http"
+)
 
-func NewService() {
-	slog.Info("Here is the newww service ...")
+type AdObject struct {
+	AdID     string `json:"ad_id"`
+	BidPrice int    `json:"bid_amount"`
+}
+
+func BidService(w http.ResponseWriter, r *http.Request) {
+	adObject := &AdObject{
+		AdID:     "AD-ID",
+		BidPrice: rand.IntN(1500) + 200,
+	}
+
+	w.Header().Set("content-type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(adObject); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
--- services/bidding/bidding_test.go
+++ services/bidding/bidding_test.go
@@ -2,6 +2,10 @@
 package bidding
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -9,27 +13,39 @@ import (
 
 func TestBiddingService(t *testing.T) {
 	Convey("Scenario - Bidding Sever receives HTTP request", t, func() {
-		Convey("Given request handler", nil)
+		Convey("Given request handler", func() {
+			Convey("Then service receives request", func() {
+				w := httptest.NewRecorder()
 
-		Convey("Then service receives request", func() {
-			Convey("When request contains AdPlacmentID", func() {
-				Convey("Then service should respond with AdOpbject", func() {
-					Convey("Then AdObject should contain AdID", nil)
+				Convey("When request contains AdPlacmentID", func() {
+					query := url.Values{"ad_placement_id": {"1234-1234-1234"}}
+					req := httptest.NewRequest(http.MethodGet, "/bid"+query.Encode(), nil)
 
-					Convey("And  AdObject should contain BidPrice ( random for now )", func() {
-						Convey("Then BidPrice should be in cents ( avoid floats for currency !!! )", nil)
+					BidService(w, req)
+					Convey("Then service should respond with JSON, AdOpbject", func() {
+						adObject := new(AdObject)
+						resp := w.Result()
+						So(resp.Header.Get("Content-Type"), ShouldEqual, "application/json")
+
+						err := json.NewDecoder(w.Body).Decode(&adObject)
+						So(err, ShouldBeNil)
+
+						Convey("Then AdObject should contain AdID", nil)
+						Convey("And  AdObject should contain BidPrice ( random for now )", func() {
+							Convey("Then BidPrice should be in cents ( avoid floats for currency !!! )", nil)
+						})
+
+						Convey("And StatusOK (200)", nil)
 					})
 
-					Convey("And StatusOK (200)", nil)
+					Convey("When service not interested in the spot", func() {
+						Convey("Then return StatusNoContent (204)", nil)
+					})
 				})
 
-				Convey("When service not interested in the spot", func() {
-					Convey("Then return StatusNoContent (204)", nil)
+				Convey("But the request doe's not contains AdPlacementID", func() {
+					Convey("Then Status Forbidden", nil)
 				})
-			})
-
-			Convey("But the request doe's not contains AdPlacementID", func() {
-				Convey("Then Status Forbidden", nil)
 			})
 		})
 	})
